35e87448a51c466d85ed8abf341ae08c3fdaaa37
Saicharangundebommu <saicharangundebommu@gmail.com>
2026-02-10 09:58:03 +0530

    first commit

--- app.py
+++ app.py
@@ -446,4 +446,5 @@ def delete_record(result_file):
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
